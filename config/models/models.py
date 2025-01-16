c9017808628d2a7578abfce23cb94958904c04b2
lfgalleguillos <92994942+lfgalleguillos@users.noreply.github.com>
2025-01-15 22:22:01 -0300

    Add files via upload

--- config/models/models.py
+++ config/models/models.py
@@ -1,5 +1,6 @@
+from flask_login import UserMixin
 from sqlalchemy.orm import relationship
-from config.db.db import db
+from ..db.db import db
 from sqlalchemy import (
     Column,
     Integer,
@@ -13,18 +14,23 @@ from sqlalchemy import (
 from datetime import datetime
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):  # Hereda de UserMixin
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     created_at = Column(DateTime, default=datetime.utcnow)
     email = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     is_admin = Column(Boolean, default=False)
     rol = Column(String, default="User")
 
     messages = relationship("Message", back_populates="user", order_by="Message.id")
     profile = relationship("Profile", back_populates="user", uselist=False)
+
+    def get_id(self):
+        """Devuelve el ID del usuario como cadena."""
+        return str(self.id)
 
 
 class Message(db.Model):
